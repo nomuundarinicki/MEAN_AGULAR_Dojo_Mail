635ed3679faf72627d7ac5e23045195c25a3704b
Nick Chimedbavuu <nickchimedbavuu@Nicks-MacBook-Pro.local>
2017-07-17 20:53:06 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  emails: Array<object> = [
+    {
+      email: 'Bill@gates.com',
+      important: true,
+      subject: 'New Windows',
+      content: 'Windows XI will launch in year 2100.'
+    },
+    {
+      email: 'Ada@lovelace.com',
+      important: true,
+      subject: 'Programming',
+      content: 'Enchantress of Numbers'
+    },
+    {
+      email: 'John@carmac.com',
+      important: false,
+      subject: 'Updated Algo',
+      content: 'New algorithm for shadow volumes.'
+    },
+    {
+      email: 'Gabe@newel.com',
+      important: false,
+      subject: 'HL3!',
+      content: 'Just kidding...'
+    }
+  ]
 }
